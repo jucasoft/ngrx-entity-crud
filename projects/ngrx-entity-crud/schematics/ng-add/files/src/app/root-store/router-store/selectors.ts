4c90056229af3cfa1c5ebb36a6b1f29081fc2979
lucalore <lluuccaa1@gmail.com>
2025-02-14 15:47:21 +0100

    Minor fixes and code improvements.

--- projects/ngrx-entity-crud/schematics/ng-add/files/src/app/root-store/router-store/selectors.ts
+++ projects/ngrx-entity-crud/schematics/ng-add/files/src/app/root-store/router-store/selectors.ts
@@ -1,8 +1,8 @@
 import {createFeatureSelector, createSelector, MemoizedSelector} from '@ngrx/store';
-import * as fromRouter from '@ngrx/router-store';
 import {Data, NavigationExtras, Params} from '@angular/router';
 import {State} from './state';
 import {evalData} from 'ngrx-entity-crud';
+import {getRouterSelectors} from "@ngrx/router-store";
 
 export const selectRouterState: MemoizedSelector<object, State> = createFeatureSelector<State>('router');
 const getOptions = (state: State): any => state?.state?.root?.firstChild?.params["options"];
@@ -31,13 +31,17 @@ export const hasPopUp: MemoizedSelector<object, NavigationExtras> = createSelect
 );
 
 export const {
-  selectQueryParams,    // select the current route query params
-  selectQueryParam,     // factory function to select a query param
-  selectRouteParams,    // select the current route params
-  selectRouteParam,     // factory function to select a route param
-  selectRouteData,      // select the current route data
-  selectUrl,            // select the current url
-} = fromRouter.getSelectors(selectRouterState);
+  selectCurrentRoute, // select the current route
+  selectFragment, // select the current route fragment
+  selectQueryParams, // select the current route query params
+  selectQueryParam, // factory function to select a query param
+  selectRouteParams, // select the current route params
+  selectRouteParam, // factory function to select a route param
+  selectRouteData, // select the current route data
+  selectRouteDataParam, // factory function to select a route data param
+  selectUrl, // select the current url
+  selectTitle, // select the title if available
+} = getRouterSelectors(selectRouterState);
 
 export const all: MemoizedSelector<object, RuoteData> = createSelector(
   selectQueryParams,
@@ -61,3 +65,7 @@ export interface RuoteData {
   selectUrl: string;
 }
 
+export const selectId = createSelector(
+  selectRouteParams,
+  (params:Params) => params['id']
+)
