3f24c260f204de667cbc0eedd1d4cb944ffadedf
jucasoft <lluuccaa1@gmail.com>
2025-09-18 16:03:22 +0200

    Migliorata gestione delle proprietà con `Object.prototype.hasOwnProperty.call` in `base-singular-crud.service.ts` per garantire maggiore sicurezza e coerenza.

--- libs/ngrx-entity-crud/src/lib/base-singular-crud.service.ts
+++ libs/ngrx-entity-crud/src/lib/base-singular-crud.service.ts
@@ -1,38 +1,44 @@
-import {Injectable} from '@angular/core';
-import {map} from 'rxjs/operators';
-import {Observable} from 'rxjs';
-import {BaseCrudService} from './base-crud.service';
-import {IBaseCrudService} from './ibase-crud-service';
-import {ICriteria, Response} from './models';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { BaseCrudService } from './base-crud.service';
+import { IBaseCrudService } from './ibase-crud-service';
+import { ICriteria, Response } from './models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BaseSingularCrudService<T> extends BaseCrudService<T> implements IBaseCrudService<T> {
-
-
+export class BaseSingularCrudService<T>
+  extends BaseCrudService<T>
+  implements IBaseCrudService<T>
+{
   select(opt: ICriteria): Observable<Response<T>> {
     console.log('BaseSingularCrudService.select()');
 
-    if (typeof (console) !== 'undefined' && this.debug) {
+    if (typeof console !== 'undefined' && this.debug) {
       console.log('BaseCrudService.search()');
       console.log('Extended from: ' + this.constructor.name);
     }
-    const url = opt && opt.hasOwnProperty('path') && !!opt.path ? opt.path.join('/') : '';
+    const url =
+      opt && Object.prototype.hasOwnProperty.call(opt, 'path') && !!opt.path
+        ? opt.path.join('/')
+        : '';
     let httpOptions = this.httpOptions();
 
-    if (opt && opt.hasOwnProperty('queryParams') && !!opt.queryParams) {
-      httpOptions = ({...httpOptions, ...{params: opt.queryParams}});
+    if (
+      opt &&
+      Object.prototype.hasOwnProperty.call(opt, 'queryParams') &&
+      !!opt.queryParams
+    ) {
+      httpOptions = { ...httpOptions, ...{ params: opt.queryParams } };
     }
 
     console.log('httpOptions', httpOptions);
     console.log('url', url);
     console.log('this.getUrl()', this.getUrl());
 
-    return this.http.get(this.getUrl() + url, httpOptions).pipe(
-      map(this.searchMap),
-    ) as Observable<Response<T>>;
-
+    return this.http
+      .get(this.getUrl() + url, httpOptions)
+      .pipe(map(this.searchMap)) as Observable<Response<T>>;
   }
-
 }
